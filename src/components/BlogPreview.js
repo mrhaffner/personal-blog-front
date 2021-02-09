1246fe9ec13983752395ea635e8829b54526fd0c
Matt Haffner <mrhaffner13@gmail.com>
2021-02-08 23:45:46 -0600

    blogs now rotate through justify-start, justify-center, justify-end in Home

--- src/components/BlogPreview.js
+++ src/components/BlogPreview.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogPreview = ({blog}) => {
     return (
-        <div className='text-white px-5 py-5 w-1/3'>
+        <div className='text-white px-5 py-5 max-w-xl'>
             <div className='text-xs font-bold tracking-wide'>{blog.date}</div>
             <div className='text-xl font-bold pb-5'>{blog.title}</div>
             <div>{blog.text}</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,12 @@ import blogs from '../blogs'
 
 const Home = () => {
     const colors = ['bg-green-700', 'bg-green-400', 'bg-pink-700', 'bg-pink-400', 'bg-blue-700', 'bg-blue-400']
-    // let i = 0
+    const justify = ['justify-start', 'justify-center', 'justify-end', 'justify-center']
     return (
         <div className='h-full w-full'>
             {  
                 blogs.map((blog, i) => {
-                    // if (i >= colors.length) i = 0
-                    // i++
-                    let classes = `${colors[i % colors.length]} h-52`
+                    let classes = `${colors[i % colors.length]} h-52 flex ${justify[i % justify.length]}`
                     return (
                         <div className={classes}  key={blog.title}>
                             <BlogPreview blog={blog}/>
