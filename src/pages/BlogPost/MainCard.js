ea51239a7ff9b9f927de9a081240f8435a517e03
Matt Haffner <mrhaffner13@gmail.com>
2021-02-15 00:58:25 -0600

    added react-markdown and gfm, as well as some dummy style rules for markdown class in index.css.  Added a dummy markdown.js to be fed into BlogPost. Added additional formatting to MainCard in BlogPost

--- src/pages/BlogPost/MainCard.js
+++ src/pages/BlogPost/MainCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
-const MainCard = () => {
+const MainCard = ({ article }) => {
     return (
         <div className='mx-5 -mt-20 bg-white shadow-custom rounded-2xl text-gray-700 flex flex-col items-center'>
-            <div className='w-278 flex flex-col'>
-                <div>article text....</div>
-                <div className='flex items-center space-x-6 border-t py-4'>
+            <div className='w-184 flex flex-col mt-16'>
+                <ReactMarkdown className='markdown' plugins={[[gfm, {singleTilde: false}]]}>{article}</ReactMarkdown>
+                <div className='flex items-center space-x-6 border-t py-4 mt-12 mb-20'>
                     <div className='bg-author bg-center bg-cover w-24 h-24 rounded-full shadow-custom'></div>
                     <div>
                         <h3 className='text-l font-semibold'>Matt Haffner</h3>
--- src/pages/BlogPost/index.js
+++ src/pages/BlogPost/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import MainCard from './MainCard'
 import Header from './Header'
 import Footer from './Footer'
+import markdown from '../../markdown'
 
 const BlogPost = () => {
   
     return (
         <div className='bg-lightblue-50 h-full pb-16'>
             <Header title='Man This Is a Really Great Title' subTitle='Lorem ipsum something something in latin' />
-            <MainCard />
+            <MainCard article={markdown} />
             <Footer />
         </div>
     );
