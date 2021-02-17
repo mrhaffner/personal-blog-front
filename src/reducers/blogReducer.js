8050dce8ad2fc4eb953d6801d4e92d4a7e0e5168
Matt Haffner <mrhaffner13@gmail.com>
2021-02-17 03:34:02 -0600

    added more routes to blogs and updated blogReducer accordingly

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -10,12 +10,34 @@ const blogReducer = (state = [], action) => {
     return state.filter(x => x.id !== action.id)
   case 'EDIT_BLOG':
     return state.map(x => x.id !== action.updatedBlog.id ? x : action.updatedBlog)
+  case 'PUBLISH_BLOG':
+    return state.map(x => x.id !== action.id ? x : { ...x, isPublished: action.publishStatus })
   default:
     return state
   }
 }
 
-export const setBlogs = () => {
+export const getPublishedBlogs = () => {
+  return async dispatch => {
+    const blogs = await blogService.getPublishedPosts()
+    dispatch ({
+      type: 'SET_BLOGS',
+      blogs
+    })
+  }
+}
+
+export const getUnpublishedBlogs = () => {
+  return async dispatch => {
+    const blogs = await blogService.getUnpublishedPosts()
+    dispatch ({
+      type: 'SET_BLOGS',
+      blogs
+    })
+  }
+}
+
+export const getAllBlogs = () => {
   return async dispatch => {
     const blogs = await blogService.getAllPosts()
     dispatch ({
@@ -24,6 +46,8 @@ export const setBlogs = () => {
     })
   }
 }
+
+//get one blog?
 
 export const addBlog = blog => {
   return async dispatch => {
@@ -35,22 +59,33 @@ export const addBlog = blog => {
   }
 }
 
-export const removeBlog = blog => {
-  return dispatch => {
-    blogService.removePost(blog.id)
-    dispatch({
-      type: 'REMOVE_BLOG',
-      id: blog.id
-    })
-  }
-}
-
 export const editBlog = (id, blog) => {
   return dispatch => {
     blogService.editPost(id, blog)
     dispatch({
       type: 'EDIT_BLOG',
       blog
+    })
+  }
+}
+
+export const publishBlog = (id, publishStatus) => {
+  return dispatch => {
+    blogService.publishPost(id, publishStatus)
+    dispatch({
+      type: 'PUBLISH_BLOG',
+      publishStatus,
+      id
+    })
+  }
+}
+
+export const removeBlog = blog => {
+  return dispatch => {
+    blogService.removePost(blog.id)
+    dispatch({
+      type: 'REMOVE_BLOG',
+      id: blog.id
     })
   }
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/blogs'
 
-const getAllPosts = async () => {
+const getPublishedPosts = async () => {
     const response = await axios.get(baseUrl)
+    return response.data
+}
+
+const getUnpublishedPosts = async () => {
+    const response = await axios.get(`${baseUrl}/unpublished`)
+    return response.data
+}
+
+const getAllPosts = async () => {
+    const response = await axios.get(`${baseUrl}/all`)
+    return response.data
+}
+
+const getPost = async id => {
+    const response = await axios.get(`${baseUrl}/${id}`)
     return response.data
 }
 
@@ -12,13 +27,18 @@ const createPost = async newPost => {
 }
 
 const editPost = async (id, editedPost) => {
-    const response = await axios.put(`${baseUrl}/${id}`, editedPost)
+    const response = await axios.put(`${baseUrl}/${id}/update`, editedPost)
+    return response.data
+}
+
+const publishPost = async (id, publishStatus) => {
+    const response = await axios.put(`${baseUrl}/${id}/publish`, publishStatus)
     return response.data
 }
 
 const removePost = async id => {
-    const response = await axios.delete(`${baseUrl}/${id}`)
+    const response = await axios.delete(`${baseUrl}/${id}/delete`)
     return response.data
-  }
+}
 
-export default { getAllPosts, createPost, editPost, removePost }
+export default { getPublishedPosts, getUnpublishedPosts, getAllPosts, getPost, createPost, editPost, publishPost, removePost }
