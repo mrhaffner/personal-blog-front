069b9f075ffec2cbce1c284dfd91bd93dc23ac86
mrhaffner <mrhaffner13@gmail.com>
2021-09-29 16:59:01 -0500

    fix: blog suggestions from empty BlogList no longer appear in Admin

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import BlogCard from './BlogCard';
 import BlogSuggest from './BlogSuggest';
 
 const BlogList = ({ blogs, edit, textFilter }) => {
   // this needs to change to handle arrays shorter than 9 - might be easy to map if using flex grid instead of all the flex box divs
   // might need to set a fixed height for the card if less than 9 blogs are given
+  let location = useLocation();
+  console.log(location);
 
   if (textFilter !== '') {
     const filteredBlogs = blogs.filter((blog) =>
@@ -17,12 +20,14 @@ const BlogList = ({ blogs, edit, textFilter }) => {
           <h3 className="text-bluegray-500 text-xl mt-36 mb-72">
             Sorry, no posts matched your search...
           </h3>
-          <div className="w-278 mb-20">
-            <h5 className="text-xl font-semibold mb-20 text-center">
-              You May Enjoy
-            </h5>
-            <BlogSuggest blogs={blogs} />
-          </div>
+          {location.pathname !== '/admin' && (
+            <div className="w-278 mb-20">
+              <h5 className="text-xl font-semibold mb-20 text-center">
+                You May Enjoy
+              </h5>
+              <BlogSuggest blogs={blogs} />
+            </div>
+          )}
         </>
       );
     }
