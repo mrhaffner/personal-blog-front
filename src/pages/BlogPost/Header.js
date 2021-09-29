89d8288a5c4940f50f89754434eb9f4e6e34b14e
mrhaffner <mrhaffner13@gmail.com>
2021-09-29 14:37:48 -0500

    style: hero image now uses bg-cover for wider screens

--- src/pages/BlogPost/Header.js
+++ src/pages/BlogPost/Header.js
@@ -3,7 +3,7 @@ import Nav from '../../components/Nav';
 
 const Header = ({ title, subTitle }) => {
   return (
-    <div className="bg-fixed h-screen text-white bg-hero-pattern">
+    <div className="bg-fixed h-screen text-white bg-hero-pattern bg-cover">
       <Nav />
       <div className="flex flex-col items-center pt-96">
         <h1 className="font-bold text-4xl pb-4">{title}</h1>
--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Nav from '../../components/Nav';
-import bgImg from '../../img/hero-pattern.jpg';
 
 const Header = () => {
   return (
-    <div
-      className="bg-fixed h-hero text-white bg-hero-pattern"
-      // style={{
-      //   background: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${bgImg}) center center`,
-      // }}
-    >
+    <div className="bg-fixed h-hero text-white bg-hero-pattern bg-cover">
       <Nav />
       <div className="flex justify-center pt-72">
         <h1 className="font-bold text-4xl">Welcome to The Blog</h1>
