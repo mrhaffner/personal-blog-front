5a3319dfe5333ea88c10786de9da489c0b8c4335
mrhaffner <mrhaffner13@gmail.com>
2021-09-29 17:24:03 -0500

    feat: BlogSuggest now randomly chooses blogs to display

--- src/components/BlogSuggest.js
+++ src/components/BlogSuggest.js
@@ -1,11 +1,15 @@
 import PreviewCard from './PreviewCard';
 
-const BlogSuggest = ({ blogs }) => (
-  <div className="flex justify-between w-full">
-    <PreviewCard bg={1} blog={blogs[0]} />
-    <PreviewCard bg={2} blog={blogs[1]} />
-    <PreviewCard bg={3} blog={blogs[2]} />
-  </div>
-);
+const BlogSuggest = ({ blogs }) => {
+  blogs.sort(() => Math.random() - 0.5);
+  return (
+    //random blogs
+    <div className="flex justify-between w-full">
+      <PreviewCard bg={1} blog={blogs[0]} />
+      <PreviewCard bg={2} blog={blogs[1]} />
+      <PreviewCard bg={3} blog={blogs[2]} />
+    </div>
+  );
+};
 
 export default BlogSuggest;
