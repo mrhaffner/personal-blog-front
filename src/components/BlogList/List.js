de051d3a0da37ad27201df38d476d27d8fbf1da0
mrhaffner <mrhaffner13@gmail.com>
2021-09-29 19:28:00 -0500

    style: changed the spacing of BlogList cards

--- src/components/BlogList/List.js
+++ src/components/BlogList/List.js
@@ -6,7 +6,7 @@ const List = ({ blogs, edit, page }) => {
   const currentBlogs = blogs.slice(begin, end);
 
   return (
-    <div className="w-278 mt-16 mb-32 grid grid-cols-3 gap-y-32 gap-x-8">
+    <div className="w-278 mt-8 mb-32 grid grid-cols-3 gap-y-24 gap-x-8">
       {currentBlogs.map((blog) => {
         return (
           <BlogCard bg={1} blog={blog} edit={edit} key={blog._id} />
