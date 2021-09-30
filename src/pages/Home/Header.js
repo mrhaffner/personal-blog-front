54ed885414e735a14017921be5f2899e6547bad5
mrhaffner <mrhaffner13@gmail.com>
2021-09-30 18:22:24 -0500

    style: added more hero-pattern images. added new hero-pattern for BlogPost

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -3,7 +3,7 @@ import Nav from '../../components/Nav';
 
 const Header = () => {
   return (
-    <div className="bg-fixed h-hero text-white bg-hero-pattern bg-cover">
+    <div className="bg-fixed h-hero text-white bg-hero-pattern1 bg-cover">
       <Nav />
       <div className="flex justify-center pt-72">
         <h1 className="font-bold text-4xl">Welcome to The Blog</h1>
