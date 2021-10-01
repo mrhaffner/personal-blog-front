19fb1119283d859e11fddeca88feaa40020cec2a
mrhaffner <mrhaffner13@gmail.com>
2021-10-01 16:43:53 -0500

    style: added icon to New Post link

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -16,8 +16,24 @@ const NavHeader = () => {
     <div className="bg-green-700 h-screen text-white">
       <nav className="flex justify-center">
         <div className="flex justify-between pt-10 w-278 font-bold text-l tracking-wide">
-          <Link to="/admin/new" className="hover:text-bluegray-200">
-            + New Post (Add Link and Icon)
+          <Link
+            to="/admin/new"
+            className="hover:text-bluegray-200 flex"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+              <path
+                fillRule="evenodd"
+                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="pl-1">New Post</span>
           </Link>
           <div className="space-x-6">
             <Link to="/admin" className="hover:text-bluegray-200">
