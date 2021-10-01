3b224a432f4379c2b8f8eb84640ff8415f8f8e7c
mrhaffner <mrhaffner13@gmail.com>
2021-10-01 14:24:57 -0500

    style: BlogHero now selects a random image to display

--- src/pages/BlogPost/Header.js
+++ src/pages/BlogPost/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Nav from '../../components/Nav';
 
+const heroStyle = `bg-fixed h-screen text-white bg-hero-pattern${
+  Math.floor(Math.random() * (Math.floor(6) - Math.ceil(2) + 1)) +
+  Math.ceil(2)
+} bg-cover`;
+
 const Header = ({ title, subTitle }) => {
   return (
-    <div className="bg-fixed h-screen text-white bg-hero-pattern2 bg-cover">
+    <div className={heroStyle}>
       <Nav />
       <div className="flex flex-col items-center pt-96">
         <h1 className="font-bold text-4xl pb-4">{title}</h1>
