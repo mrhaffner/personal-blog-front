48e4a5e1a424b50d9626e43bf41ea328435c3e21
mrhaffner <mrhaffner13@gmail.com>
2021-10-02 17:26:15 -0500

    style: updated margins and sizing for ChangePostAlert

--- src/components/ChangePostAlert.js
+++ src/components/ChangePostAlert.js
@@ -3,15 +3,11 @@ import FormButton from './FormButton';
 const ChangePostAlert = ({ alertText, toggleAlert, submitFn }) => {
   return (
     <div className="absolute left-0 top-0 z-50 flex justify-center items-start h-screen w-screen bg-black bg-opacity-75">
-      <div className="h-48 w-96 mt-64 bg-white flex justify-center items-center flex-col rounded">
-        <h3>Are you sure you want to {alertText} your post?</h3>
-        <div className="space-x-6">
-          {/* <button onClick={submitFn} className="">
-            Confirm
-          </button>
-          <button onClick={toggleAlert} className="">
-            Return
-          </button> */}
+      <div className="h-52 w-104 mt-64 bg-white flex items-center flex-col rounded">
+        <h3 className="text-gray-600 mt-16 mb-12 font-semibold">
+          Are you sure you want to {alertText} your post?
+        </h3>
+        <div className="space-x-28">
           <FormButton
             clickFn={submitFn}
             text="Confirm"
