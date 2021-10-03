828e2e0566f9e419165366b044f4e2f57aafea2c
mrhaffner <mrhaffner13@gmail.com>
2021-10-03 18:21:13 -0500

    fix: added cursor pointer and select-none to picture card

--- src/pages/Home/PictureCard.js
+++ src/pages/Home/PictureCard.js
@@ -6,9 +6,9 @@ const PictureCard = ({ bg, blog }) => {
   const imgClass = `w-full h-full bg-picture-card${bg} card-zoom-image`;
 
   return (
-    <div className=" w-1/4 h-96 card-zoom">
+    <div className=" w-1/4 h-96 card-zoom select-none">
       <Link className={imgClass} to={linkUrl} />
-      <h3 className="text-xl font-semibold text-white card-zoom-text">
+      <h3 className="text-xl font-semibold text-white card-zoom-text cursor-pointer">
         {blog.title}
       </h3>
     </div>
